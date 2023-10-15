0d52bcd7cfaf326a57459ab1779cff24b900cf0d
Ismoh <12631485+Ismoh@users.noreply.github.com>
2023-10-15 16:19:27 +0200

    Fixed .gitignore

--- mods/noita-vscode-debugger/lua.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-IF "%*"=="" (set I=-i) ELSE (set I=)
-set "LUAROCKS_SYSCONFDIR=C:\Program Files (x86)/luarocks"
-"M:\_porgramming\noita-vscode-debugger\.build\LuaJIT-2.1.0-beta3\bin\luajit.exe" -e "package.path=\"C:\\Users\\Ismoh-PC\\AppData\\Roaming/luarocks/share/lua/5.1/?.lua;C:\\Users\\Ismoh-PC\\AppData\\Roaming/luarocks/share/lua/5.1/?/init.lua;\"..package.path;package.cpath=\"C:\\Users\\Ismoh-PC\\AppData\\Roaming/luarocks/lib/lua/5.1/?.dll;\"..package.cpath" %I% %*
-exit /b %ERRORLEVEL%
--- mods/noita-vscode-debugger/luarocks.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-set "LUAROCKS_SYSCONFDIR=C:\Program Files (x86)/luarocks"
-"M:\_porgramming\noita-vscode-debugger\.build\luarocks-3.9.2-windows-32\luarocks.exe" --project-tree M:\_porgramming\noita-vscode-debugger\mods\noita-vscode-debugger/lua_modules %*
-exit /b %ERRORLEVEL%
